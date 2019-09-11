7fe063feae8288ead108592b71f95600594b3b26
Kristina Davis <krictis@gmail.com>
2019-09-11 16:04:22 -0400

    added self portrait

--- sketch.js
+++ sketch.js
@@ -1,12 +1,26 @@
 function setup() {
-  createCanvas(1500, 1500);
+  createCanvas(640, 640);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+  background(230);
+  ellipseMode(CENTER);
+  ellipseMode(CENTER);
+
+  //head
+  fill(247,213,213);
+  ellipse(150,150,150,150);
+  
+  //eyes
+  fill(42,123,229);
+  ellipse(110,140,20,20);
+  ellipse(186,140,20,20);
+  
+
+  //nose
+  fill(289,198,198)
+  ellipse(149,160,25,10);
+ 
+  //mouse
+  line(130,193,170,193);
 }
