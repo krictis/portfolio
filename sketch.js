ead7dc4f04ac6f6a4f6016f0e8cec560e6f26fa3
Kristina <kristina@viktor-onyshchenko-mbp.fios-router.home>
2019-09-12 23:28:08 -0400

    Added self portrait

--- sketch.js
+++ sketch.js
@@ -3,24 +3,41 @@ function setup() {
 }
 
 function draw() {
-  background(230);
-  ellipseMode(CENTER);
-  ellipseMode(CENTER);
+  background(100);
+  
 
   //head
   fill(247,213,213);
-  ellipse(150,150,150,150);
+  ellipse(150,150,150,170);
   
+  //eyebrows
+  fill(105,105,105);
+  noStroke(0);
+  ellipse(112,118,25,8);
+  ellipse(185,118,25,8);
+
   //eyes
-  fill(42,123,229);
-  ellipse(110,140,20,20);
-  ellipse(186,140,20,20);
+  fill(255);
+  stroke(108,101,94);
+  ellipse(110,140,25,20);
+  ellipse(186,140,25,20);
+  
+  //zrachok
+  fill(0);
+  stroke(30,144,255);
+  strokeWeight(2);
+  ellipse(110,140,8,8);//left
+  ellipse(185,140,8,8);//right
   
 
   //nose
-  fill(289,198,198)
-  ellipse(149,160,25,10);
+  fill(289,198,198);
+  stroke(235,149,149);
+  ellipse(149,160,20,10);
  
   //mouse
   line(130,193,170,193);
+  arc(150, 193, 40, 30, 0, PI);
+  
 }
+  
