20568ac8edde280a6fa90cee0ee6bd46bd49a119
Kristina <kristina@viktor-onyshchenko-mbp.fios-router.home>
2019-09-16 15:49:39 -0400

    Added self portrait

--- sketch.js
+++ sketch.js
@@ -1,20 +1,39 @@
+
+var r=0;
+var b=255;
+
+var oval1= {
+  x:118,
+  y:118,
+  diametr:30
+};
+
+var oval2= {
+  x:185,
+  y:118,
+  diametr:30
+};
+
 function setup() {
-  createCanvas(640, 640);
+  createCanvas(1400, 1400);
 }
 
 function draw() {
-  background(100);
-  
+  r=map(mouseX,0,600,0,255);
+  background(r,0,b);
+  oval1.x=oval1.x-1;
+  oval2.y=oval2.y-1;
+ 
 
   //head
   fill(247,213,213);
   ellipse(150,150,150,170);
   
-  //eyebrows
+  //EYEBROWS
   fill(105,105,105);
   noStroke(0);
-  ellipse(112,118,25,8);
-  ellipse(185,118,25,8);
+  ellipse(oval1.x,oval1.y,oval1.diametr,8);
+  ellipse(oval2.x,oval2.y,oval2.diametr,8);
 
   //eyes
   fill(255);
@@ -35,9 +54,10 @@ function draw() {
   stroke(235,149,149);
   ellipse(149,160,20,10);
  
-  //mouse
+  //mouth
   line(130,193,170,193);
   arc(150, 193, 40, 30, 0, PI);
   
 }
+  
   
